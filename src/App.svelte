8ea2661c1ae13f57cd8e63c60743bd25515bfdc1
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2022-03-22 18:58:43 +0100

    default colleciton id is 1

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Guesser from "./components/Guesser.svelte";
   var url = new URLSearchParams(window.location.search);
-  let collectionId = parseInt(url.get("collectionId")) | 0;
+  let collectionId = parseInt(url.get("collectionId")) | 1;
 </script>
 
 <main>
