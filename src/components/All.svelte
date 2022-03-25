6de96a578992a29a6e4bb5334ddaa82b2fc50c1c
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2022-03-25 13:27:10 +0100

    Create All.svelte

---
+++ src/components/All.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export var collectionId: number;
+    import API from "../scripts/api";
+
+    let els: API.Element[] = [];
+    const p = API.Poznvackus.fetch().then(async (p) => {
+        const c = await p.getCollection(collectionId);
+        const elsp: Promise<API.Element>[] = [];
+        for (let i = 0; i < c.elementCount; i++) {
+            elsp.push(c.getElement(i));
+        }
+        els = await Promise.all(elsp);
+    });
+</script>
+
+<main>
+    <ul>
+        {#each els as el}
+            <div>
+                <h2>{el.name}</h2>
+                <img src={el.getRandomImage()} alt="" />
+            </div>
+        {/each}
+    </ul>
+</main>
