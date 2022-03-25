f3aafec303879465770cd675c7f06f09bdb03e06
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2022-03-25 13:30:02 +0100

    fix: false / true string error

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
   import Guesser from "./components/Guesser.svelte";
+  import All from "./components/All.svelte";
   var url = new URLSearchParams(window.location.search);
   let collectionId = parseInt(url.get("collectionId")) | 1;
+  let all = url.get("all") === "true"
 </script>
 
 <main>
+  {#if !all}
   <Guesser {collectionId}/>
+  {/if}
+  {#if all}
+  <All {collectionId}/>
+  {/if}
 </main>
 
 <style>
