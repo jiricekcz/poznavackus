38b6db4ea85f4ce56bd0bd7d150017a6120474b1
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2022-06-01 21:33:17 +0200

    not a dumber syntax error exists

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import Guesser from "./components/Guesser.svelte";
   import All from "./components/All.svelte";
   var url = new URLSearchParams(window.location.search);
-  let collectionId = parseInt(url.get("collectionId")) | 2;
+  let collectionId = parseInt(url.get("collectionId")) || 2;
   let all = url.get("all") === "true"
 </script>
 
